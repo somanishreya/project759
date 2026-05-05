71d4ca05dd5942b90976d69f821d846b200a6d87
Shreya Somani <somanishreya99@gmail.com>
2026-05-05 04:40:03 -0500

    Clean submission branch: remove gem5 source tree, keep only FinalProject

--- ext/dramsys/cmake/FindSystemCLanguage.cmake
+++
@@ -1,9 +0,0 @@
-if (NOT SystemCLanguage_FOUND)
-    add_library(systemc INTERFACE)
-
-    target_include_directories(systemc INTERFACE "${SCONS_SOURCE_DIR}/src/systemc/ext/systemc_home/include")
-
-    add_library(SystemC::systemc ALIAS systemc)
-    
-    set(SystemCLanguage_FOUND TRUE)
-endif()
--- tests/gem5/readfile_tests/configs/test_script.sh
+++
@@ -1,2 +0,0 @@
-echo "running test script from readfile_tests"
-sleep 5 #sleeping for 5 seconds to let gem5 write terminal output to output directory
